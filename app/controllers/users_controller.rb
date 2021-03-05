babb0d43daec12f21585ec49bed61ba7527216d6
raw7330 <ryosuke.bdc@gmail.com>
2021-03-05 09:14:56 +0000

    add user/show,edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
    
   def show
+    @user = User.find(params[:id])
   end
   
   def edit
   end
   
   def update
+    if current_user.update(user_params)
+      redirect_to user_params
+    else
+      render :edit
+    end
   end
   
   def quit
   end
   
   def out
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:company_name, :company_name_kana, :postal_code, :address, :phone_number, :is_deleted )
   end
   
 end
