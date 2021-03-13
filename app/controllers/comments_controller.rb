93a8ec9a50cfa888a9a9afc8056bbb2399fcf236
raw7330 <ryosuke.bdc@gmail.com>
2021-03-13 03:11:24 +0000

    add 非同期通信

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
-  
+
   def create
     @item = Item.find(params[:item_id])
     @comment = @item.comments.new(comment_params)
     @comment.user_id = current_user.id
-    
+
     if @comment.save
       flash[:notice] = 'コメント完了しました'
       redirect_back(fallback_location: root_path)
@@ -13,14 +13,14 @@ class CommentsController < ApplicationController
       redirect_back(fallback_location: root_path)
     end
   end
-  
+
   def destroy
     @item = Item.find(params[:item_id])
     @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to item_path(params[:item_id])
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:comment)
